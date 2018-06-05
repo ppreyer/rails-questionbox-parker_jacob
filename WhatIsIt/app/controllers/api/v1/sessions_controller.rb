cdfc46932b61ec27e0480d45cda12edb7c5dc59f
Jacobfh <jfh708@gmail.com>
2018-06-05 17:37:35 -0400

    unnested questions in routes, application layout created for header, progress on user token authentication and registration

--- WhatIsIt/app/controllers/api/v1/sessions_controller.rb
+++ WhatIsIt/app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,14 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+    skip_before_action :verify_authentication
+
+    def create
+      user = User.find_by(username: params[:username])
+  
+      if user && user.authenticate(params[:password])
+        redirect_to api_v1_questions_path
+        render json: { token: user.api_token }
+      else
+        render json: { error: "Invalid credentials" }, status: :unauthorized
+      end
+    end
 end
--- WhatIsIt/config/routes.rb
+++ WhatIsIt/config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users do
+      resources :users
+      root 'questions#index'
+      resources :questions do
         member do
-          resources :questions do
-            member do
-              resources :answers
-            end
-        end    
+          resources :answers
+        end
+      end 
     end
   end
-end
-end
   
   get 'session', to: 'sessions#new'
   post 'session', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  
-  root 'questions#index'
 end
