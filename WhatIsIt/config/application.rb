e5533cd01191ce9280dd928c2e6a361027311f59
Parker <parker.preyer1@gmail.com>
2018-06-11 22:40:49 -0400

    adds delete to CORS in app/config

--- WhatIsIt/config/application.rb
+++ WhatIsIt/config/application.rb
@@ -15,7 +15,7 @@ module WhatIsIt
    config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :options]
+       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
      end
    end
 
